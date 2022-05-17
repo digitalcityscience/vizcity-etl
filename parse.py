a01ed665191b6b44e8497ae99b0c07b142c4a808
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-05-17 15:28:05 +0200

    feat: add timestamp to the parsed result

--- parse.py
+++ parse.py
@@ -10,11 +10,12 @@ class EvChargingStationEvent:
     address: str
     latitude: float
     longitude: float
+    timestamp: int
 
 
 def extract_ev_charging_events(json_data: str) -> List[EvChargingStationEvent]:
     result = jmespath.search(
-        "value[*].{status:Datastreams[0].Observations[0].result, longitude: Locations[0].location.geometry.coordinates[0], latitude: Locations[0].location.geometry.coordinates[1], address:Locations[0].description}",
+        "value[*].{status:Datastreams[0].Observations[0].result, longitude: Locations[0].location.geometry.coordinates[0], latitude: Locations[0].location.geometry.coordinates[1], address:Locations[0].description, timestamp:Datastreams[0].Observations[0].phenomenonTime}",
         json_data,
     )
     return result  # type: ignore
