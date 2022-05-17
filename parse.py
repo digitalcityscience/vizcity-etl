d899f1635af4929234e9b33ec587063ddb255459
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-05-17 20:56:45 +0200

    chore: rename coordinate fields, update request to get only the newest dates

--- parse.py
+++ parse.py
@@ -8,14 +8,14 @@ import jmespath
 class EvChargingStationEvent:
     status: str
     address: str
-    latitude: float
-    longitude: float
+    lat: float
+    lon: float
     timestamp: int
 
 
 def extract_ev_charging_events(json_data: str) -> List[EvChargingStationEvent]:
     result = jmespath.search(
-        "value[*].{status:Datastreams[0].Observations[0].result, longitude: Locations[0].location.geometry.coordinates[0], latitude: Locations[0].location.geometry.coordinates[1], address:Locations[0].description, timestamp:Datastreams[0].Observations[0].phenomenonTime}",
+        "value[*].{status:Datastreams[0].Observations[0].result, lon: Locations[0].location.geometry.coordinates[0], lat: Locations[0].location.geometry.coordinates[1], address:Locations[0].description, timestamp:Datastreams[0].Observations[0].phenomenonTime}",
         json_data,
     )
     return result  # type: ignore
