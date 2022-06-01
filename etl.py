094575ca45aeaa2052c1226ce0794f315c78e441
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-06-01 10:37:41 +0200

    feat: add a catch all exception for geoportal related etl errors

--- etl.py
+++ etl.py
@@ -21,17 +21,21 @@ AIRPORT_API_KEY = os.getenv("AIRPORT_API_KEY", "NO_KEY_PROVIDED")
 def fetch_and_transform_geoportal_events(
     bucket: str, url: str, extract_function: Callable, json=False
 ):
-    print(f"Collecting remote data from {url} ...")
-    request = requests.get(url)
-    result_xml = request.json() if json else request.text
-    print(f"Parsing events {result_xml} ...")
-    events = extract_function(result_xml)
-    print(events)
-    events_points = [event.to_point() for event in events]
-    print(
-        f"Writing to timeseries db {[event.to_line_protocol() for event in events_points]} ..."
-    )
-    write_points_to_influx(bucket, events_points)
+    try:
+        print("No group number given! Will use the default group 1")
+        print(f"Collecting remote data from {url} ...")
+        request = requests.get(url)
+        result_xml = request.json() if json else request.text
+        print(f"Parsing events {result_xml} ...")
+        events = extract_function(result_xml)
+        print(events)
+        events_points = [event.to_point() for event in events]
+        print(
+            f"Writing to timeseries db {[event.to_line_protocol() for event in events_points]} ..."
+        )
+        write_points_to_influx(bucket, events_points)
+    except:
+        print("Something went wrong while doing: fetch_and_transform_geoportal_events")
 
 
 def collect_stadtrad(bucket: str):
