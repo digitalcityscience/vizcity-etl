5f9d3338308f2d6a1d7c1443a5af7ad88b545a44
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-06-30 07:08:08 +0200

    feat: add weather data to team gotham's bucket

--- etl_gotham.py
+++ etl_gotham.py
@@ -1,4 +1,5 @@
-from etl import collect_stadtrad, collect_swis
+from etl import collect_stadtrad, collect_swis, collect_weather_dwd
+from models import DWDWeatherStation, Location
 
 
 BUCKET = "gotham"
@@ -7,3 +8,10 @@ BUCKET = "gotham"
 def collect():
     collect_stadtrad(BUCKET)
     collect_swis(BUCKET)
+    dwd_hamburg = DWDWeatherStation(
+        location=Location(lat=53.38, lon=10.00),
+        id="10147",
+        name="HAMBURG-FU",
+        elevation=16,
+    )
+    collect_weather_dwd(dwd_hamburg, BUCKET)
