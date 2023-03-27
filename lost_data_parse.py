b2fdecab4fa50c7f78820fdb7725d108dac95c13
Andre@HCU <andre.landwehr@hcu-hamburg.de>
2023-03-27 13:46:06 +0200

    fix: counted_traffic to float

--- lost_data_parse.py
+++ lost_data_parse.py
@@ -58,7 +58,7 @@ def extract_weather():
         weather_data.append(
             WeatherConditions(
                 precipitation=float(line[4]),
-                temperature=int(line[1]),
+                temperature=int(line[1])    ,
                 pressure=line[9],
                 wind_speed=line[7],
                 timestamp=datetime.datetime.fromisoformat(line[0]),
